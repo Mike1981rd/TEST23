bf8b3a0a87a766cb846d310b816afe869d7a9e70
pilig <pilig.del07@gmail.com>
2024-09-21 22:41:29 -0600

    first try printer service

---
+++ AuroraPOS/Controllers/PrinterController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AuroraPOS.Data;
+using AuroraPOS.Models;
+using AuroraPOS.Services;
+using AuroraPOS.ViewModels;
+using AuroraPOS.Hubs;
+using Microsoft.AspNetCore.SignalR;
+using FastReport;
+
+
+namespace AuroraPOS.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    public class PrinterController : ControllerBase
+    {
+        private readonly IHubContext<PrintHub> _hubContext;
+        private readonly PrinterService _printerService;
+
+        public PrinterController(IHubContext<PrintHub> hubContext, PrinterService printerService)
+        {
+            _hubContext = hubContext;
+            _printerService = printerService;
+        }
+
+        // Este método será llamado cuando haya un trabajo de impresión pendiente
+        [HttpPost]
+        public async Task<IActionResult> NotifyPrint([FromBody] PrintJob job)
+        {
+            if (job == null || string.IsNullOrEmpty(job.PrinterName) || string.IsNullOrEmpty(job.HtmlContent))
+            {
+                return BadRequest("Datos de impresión inválidos.");
+            }
+
+            // Procesa el trabajo de impresión
+            await _printerService.ProcessPrintJob(job.PrinterName, job.HtmlContent);
+
+            // Notifica a los clientes conectados que el trabajo de impresión se completó
+            await _hubContext.Clients.All.SendAsync("PrintComplete", job.PrinterName);
+
+            return Ok(new { message = "Impresión completada", printerName = job.PrinterName });
+        }
+
+
+    }
+
+}
+
---
+++ AuroraPOS/Models/PrintJob.cs
@@ -0,0 +1,8 @@
+﻿namespace AuroraPOS.Models
+{
+    public class PrintJob
+    {
+        public string PrinterName { get; set; }
+        public string HtmlContent { get; set; }
+    }
+}
