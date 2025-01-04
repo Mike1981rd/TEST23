355d4b564fc3fbaadb3bf4f49ed5086a46a07c5e
Damagd <75856639+Damagd@users.noreply.github.com>
2025-01-04 15:21:58 -0600

    Station
    Added Station route

--- AuroraPOS/ControllersJWT/POSController.cs
+++ AuroraPOS/ControllersJWT/POSController.cs
@@ -31,7 +31,137 @@ public class POSController : Controller
         _printService = printService;
         _context = context;
     }
-    
+
+    [HttpGet("Station")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public JsonResult Station(OrderType orderType, int stationId, OrderMode mode = OrderMode.Standard, long orderId = 0, long areaObject = 0, int person = 0)
+    {
+        var stationID = stationId; // HttpContext.Session.GetInt32("StationID");
+        var station = _dbContext.Stations.Include(s => s.Areas.Where(t => !t.IsDeleted)).ThenInclude(s => s.AreaObjects.Where(s => s.ObjectType == AreaObjectType.Table && !s.IsDeleted)).FirstOrDefault(s => s.ID == stationID);
+        ViewBag.Sucursal = station.IDSucursal;
+        var order = new Order();
+
+        POSStationResponse response = new POSStationResponse();
+
+        if (station == null)
+        {
+            response.status = "station is null";
+            response.redirectTo = "Login";
+
+            return Json(response);
+        }
+
+        if (station.SalesMode == SalesMode.Barcode)
+        {
+            response.status = "station salesmode equals to barcode salesmode";
+            response.redirectTo = "Barcode";
+
+            return Json(response);
+        }
+        else if (station.SalesMode == SalesMode.Kiosk)
+        {
+            response.status = "station salesmode equals to kiosk salesmode";
+            response.redirectTo = "Kiosk";
+
+            return Json(response);
+        }
+
+        ViewBag.StationName = HttpContext.Session.GetString("StationName");
+
+        var denominations = _dbContext.Denominations.OrderByDescending(s => s.Amount).ToList();
+        ViewBag.Denominations = denominations;
+
+        var paymentMethods = _dbContext.PaymentMethods.Where(s => s.IsActive).ToList();
+        ViewBag.PaymentMethods = paymentMethods;
+
+        ViewBag.Station = station.Name;
+        ViewBag.Username = User.Identity.GetUserName();
+
+        ViewBag.OtherUsers = _dbContext.User.Where(s => s.Username != User.Identity.GetUserName()).ToList();
+        ViewBag.Areas = station.Areas;
+
+        ViewBag.ShowExpectedPayment = PermissionChecker("Permission.POS.ShowExpectedPayment");
+        ViewBag.Branchs = _dbContext.t_sucursal.ToList();
+
+        var current = _dbContext.Orders.Include(s => s.Table).FirstOrDefault(s => s.ID == orderId);
+        if (current != null)
+        {
+            order = current;
+            var name = HttpContext.User.Identity.GetUserName();
+            if (order.WaiterName != name)
+            {
+                var claims = User.Claims.Where(x => x.Type == "Permission" && x.Value == "Permission.POS.OtherOrder" &&
+                                                        x.Issuer == "LOCAL AUTHORITY");
+                if (!claims.Any())
+                {
+                    response.redirectTo = "Station";
+                    response.error = "Permission";
+
+                    return Json(response);
+                }
+            }
+
+
+        }
+        else
+        {
+            var user = HttpContext.User.Identity.GetUserName();
+            order.Station = station;
+            order.WaiterName = user;
+            order.OrderMode = OrderMode.Standard;
+            order.Status = OrderStatus.Temp;
+            var voucher = _dbContext.Vouchers.FirstOrDefault(s => s.IsPrimary);
+            order.ComprobantesID = voucher.ID;
+            if (orderType == OrderType.DiningRoom && areaObject > 0)
+            {
+                var table = _dbContext.AreaObjects.Include(s => s.Area).ThenInclude(s => s.AreaObjects.Where(s => !s.IsDeleted)).FirstOrDefault(s => s.ID == areaObject);
+                if (table != null)
+                {
+                    order.Table = table;
+                    order.Area = table.Area;
+
+                    ViewBag.AnotherTables = table.Area.AreaObjects.Where(s => s.ObjectType == AreaObjectType.Table && s.IsActive && s.ID != areaObject).ToList();
+                }
+
+                order.Person = person;
+            }
+
+
+            if (orderType == OrderType.Delivery || orderType == OrderType.FastExpress || orderType == OrderType.TakeAway)
+            {
+                order.Person = person;
+            }
+
+
+            if (orderType != OrderType.DiningRoom)
+            {
+                order.OrderMode = OrderMode.Standard;
+            }
+
+            _dbContext.Orders.Add(order);
+            _dbContext.SaveChanges();
+
+            HttpContext.Session.SetInt32("CurrentOrderID", (int)order.ID);
+        }
+
+        response.status = "OK";
+        response.order = order;
+
+        return Json(response);
+    }
+
+    public bool PermissionChecker(string permission)
+    {
+
+        var claims = User.Claims;
+        var permissionss = claims.Where(x => x.Type == "Permission" && x.Value == permission &&
+                                                        x.Issuer == "LOCAL AUTHORITY");
+
+        var valid = permissionss.Any();
+
+        return valid;
+    }
+
     [HttpPost("GetArea")]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public JsonResult GetArea(long areaID)
---
+++ AuroraPOS/ModelsJWT/POSStationResponse.cs
@@ -0,0 +1,12 @@
+﻿using AuroraPOS.Models;
+
+namespace AuroraPOS.ModelsJWT
+{
+    public class POSStationResponse
+    {
+        public string redirectTo { get; set; }
+        public string status { get; set; }
+        public Order order { get; set; }
+        public string error { get; set; }
+    }
+}
