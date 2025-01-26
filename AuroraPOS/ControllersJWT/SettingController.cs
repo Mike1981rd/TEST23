3ac2b77296554c39a0926ffe7cc42daee6eb4961
FreelancerTCMX <80782545+FreelancerTCMX@users.noreply.github.com>
2025-01-26 17:35:12 -0500

    fix
    fix

--- AuroraPOS/ControllersJWT/SettingController.cs
+++ AuroraPOS/ControllersJWT/SettingController.cs
@@ -110,7 +110,7 @@ namespace AuroraPOS.ControllersJWT
             try
             {
                 var stations = _dbContext.Stations
-                    .Select(s => new Station { ID = s.ID, Name = s.Name })
+                    .Select(s => new Station { ID = s.ID, Name = s.Name, SalesMode = s.SalesMode})
                     .ToList();
 
                 if (stations.Any() && soloTipoKiosco)
